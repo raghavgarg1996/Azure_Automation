c461fcb7d4c45c2badda162b81f0e41e698465e1
Raghav.Garg <raghav.garg@ril.com>
2022-12-28 15:50:44 +0530

    Updating commented code

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -16,14 +16,14 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-	/*	System.out.println("Hello Guys");
+		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.dc.com/characters/batman");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("Batman"));
-		driver.close();   */
+		driver.close();
 	
 		
 		
